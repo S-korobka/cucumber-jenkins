b0da4a60e53f795e9132f1521d35113ad6de63a4
S-korobka <111728423+S-korobka@users.noreply.github.com>
2022-11-17 20:11:17 +0200

    fix3

--- src/main/java/org/example/driver/DriverManager.java
+++ src/main/java/org/example/driver/DriverManager.java
@@ -17,7 +17,7 @@ public class DriverManager {
 
     private static final long IMPLICIT_WAIT_TIMEOUT = 2;
     private static final long PAGE_LOAD_TIMEOUT = 10;
-    private static final String SELENOID_HUB = "http://78.26.209.95:4444/wd/hub";
+    private static final String SELENOID_HUB = "http://192.168.56.103:4444/wd/hub/";
     private static final String BROWSER = System.getProperty("browser", "firefox");
     public static ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
 
